2d695a88e8aa6a5908994fd5f65a8052b5e4c0bd
thefirstspine <teddy@coretizone.com>
2020-01-30 22:24:45 +0100

    Added index & status pages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestController } from './rest/rest.controller';
+import { IndexController } from './index/index.controller';
 
 @Module({
   imports: [],
-  controllers: [RestController],
+  controllers: [RestController, IndexController],
   providers: [],
 })
 export class AppModule {}
