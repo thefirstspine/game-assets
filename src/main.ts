e9ea58a6f10ab2b0feeb60a7e10e05ca982c62cc
thefirstspine <teddy@coretizone.com>
2020-01-31 08:17:08 +0100

    Fixed port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import env from './@shared/env-shared/env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(env.dist ? env.config.REST_PORT : 2405);
+  await app.listen(env.dist ? env.config.PORT : 2405);
 }
 bootstrap();
