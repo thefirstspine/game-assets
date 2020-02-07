17b575ccae39ffa88c9d74c8958635a7733202f3
thefirstspine <teddy@coretizone.com>
2020-02-07 09:29:34 +0100

    Added FPE game type

---
+++ src/libraries/games-types/fpe.game-type.ts
@@ -0,0 +1,37 @@
+import { IGameType } from '../../@shared/rest-shared/entities';
+import { CyclesLibrary } from '../cycles.library';
+
+// tslint:disable: max-line-length
+export default function(): IGameType {
+  const fpeGameType: IGameType = {
+    id: 'fpe',
+    name: {
+      fr: ``,
+      en: ``,
+    },
+    description: {
+      fr: ``,
+      en: ``,
+    },
+    matchmakingMode: 'asap',
+    destinies: ['hunter'],
+    origins: [],
+    players: [
+      { x: 3, y: 0 },
+    ],
+    loots: {
+      victory: [
+      ],
+      defeat: [
+      ],
+    },
+    space: {
+      minX: 0,
+      minY: 0,
+      maxX: 6,
+      maxY: 6,
+    },
+  };
+
+  return fpeGameType;
+}
