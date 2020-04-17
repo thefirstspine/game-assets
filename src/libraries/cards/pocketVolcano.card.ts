3158a0df24e40b480d8d2aed673500596b3164e5
thefirstspine <teddy@coretizone.com>
2020-04-17 13:18:46 +0200

    Added Architect origin

---
+++ src/libraries/cards/pocketVolcano.card.ts
@@ -0,0 +1,42 @@
+import { ICard } from '../../@shared/rest-shared/card';
+
+// tslint:disable: max-line-length
+const pocketVolcanoCard: ICard = {
+  id: `pocket-volcano`,
+  text: {
+    en: ``,
+    fr: `Lorsque cette carte est détruite, placez une case Lave à la place`,
+  },
+  lore: {
+    en: ``,
+    fr: ``,
+  },
+  imageUrl: `https://static.thefirstspine.fr/placeholder.png`,
+  name: {
+    en: ``,
+    fr: `Volcan de Poche`,
+  },
+  stats: {
+    life: 4,
+    capacities: ['grow'],
+    bottom: {
+      defense: 2,
+      strength: 0,
+    },
+    left: {
+      defense: 1,
+      strength: 1,
+    },
+    right: {
+      defense: 1,
+      strength: 1,
+    },
+    top: {
+      defense: 0,
+      strength: 2,
+    },
+  },
+  type: 'artifact',
+};
+
+export default pocketVolcanoCard;
---
+++ src/libraries/cards/the-void.card.ts
@@ -0,0 +1,22 @@
+import { ICard } from '../../@shared/rest-shared/card';
+
+// tslint:disable: max-line-length
+const theVoidCard: ICard = {
+  id: `the-void`,
+  text: {
+    en: ``,
+    fr: `Détruisez une {creature}créature{/creature} ou un {artifact}artefact{/artifact} et placez une case Fosse à la place.`,
+  },
+  lore: {
+    en: ``,
+    fr: ``,
+  },
+  imageUrl: `https://static.thefirstspine.fr/placeholder.png`,
+  name: {
+    en: ``,
+    fr: `Le Vide`,
+  },
+  type: 'spell',
+};
+
+export default theVoidCard;
