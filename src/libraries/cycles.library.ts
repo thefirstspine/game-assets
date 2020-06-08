cd0aeffb26fe24e123e3b245c463f9d1eb7a014b
thefirstspine <teddy@coretizone.com>
2020-06-08 20:43:53 +0200

    Translated cycle & reverted test commit

--- src/libraries/cycles.library.ts
+++ src/libraries/cycles.library.ts
@@ -8,14 +8,14 @@ export class CyclesLibrary {
       id: `renewal-2020`,
       name: {
         fr: `Cycle du renouveau 2020`,
-        en: ``,
+        en: `Cycle of the Renewal 2020`,
       },
       description: {
         fr: `Arena fait peau neuve et lance son premier cycle, le Cycle du Renouveau 2020 !`,
-        en: ``,
+        en: `Arena is getting a facelift and is launching its first cycle, the Cycle of the Renewal 2020!`,
       },
       timestampFrom: 0,
-      timestampTo: 0,
+      timestampTo: (new Date('2020-05-01 14:59:59')).getTime() / 1000,
       rewardsForPlayers: [
         {name: 'holo-soul-of-a-sacrified-hunter', num: 1},
       ],
@@ -28,14 +28,14 @@ export class CyclesLibrary {
       id: `great-ancient-2020`,
       name: {
         fr: `Cycle du Grand Ancien 2020`,
-        en: ``,
+        en: `Cycle of the Great Ancient 2020`,
       },
       description: {
         fr: `Les Grands Anciens sont à la fête ! Célébrez ces créatures disparues dans un tournoi sans pitié et remportez la carte prémium de l'invocateur.`,
-        en: ``,
+        en: `We are celebrating the Great Ancients! Celebrate these vanished creatures in a merciless tournament and win the summoner's premium card`,
       },
-      timestampFrom: 0,
-      timestampTo: 0,
+      timestampFrom: (new Date('2020-05-01 15:00:00')).getTime() / 1000,
+      timestampTo: (new Date('2020-06-12 14:59:59')).getTime() / 1000,
       rewardsForPlayers: [
         {name: 'holo-deadly-viper', num: 1},
       ],
@@ -48,13 +48,13 @@ export class CyclesLibrary {
       id: `treasure-2020`,
       name: {
         fr: `Cycle du Trésor 2020`,
-        en: ``,
+        en: `Cycle of the Treasure 2020`,
       },
       description: {
         fr: `Des mers du sud nous sont parvenus les corsaires sanguinaires et sans scrupule. Ils recherchent de mystérieux galions d'or disséminés dans tout le Royaume. Vous pouvez en récolter en tournoi ou en partie classique, et les échanger dans la boutique !`,
-        en: ``,
+        en: `Bloodthirsty and unscrupulous corsairs reached us from their southern seas. They are looking for mysterious golden galleons scattered throughout the Kingdom. You can collect them in a tournament or in a classic game, and exchange them in the shop!`,
       },
-      timestampFrom: 0,
+      timestampFrom: (new Date('2020-06-12 15:00:00')).getTime() / 1000,
       timestampTo: (new Date('2020-07-24 14:59:59')).getTime() / 1000,
       rewardsForPlayers: [
         {name: 'holo-shadows-banner', num: 1},
