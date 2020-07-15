45700bd713d2bdf26982adf7ae04e68d7429c816
thefirstspine <teddy@coretizone.com>
2020-07-15 18:54:40 +0200

    Added new game types

--- src/libraries/games-types.library.ts
+++ src/libraries/games-types.library.ts
@@ -3,16 +3,18 @@ import classicGameType from './games-types/classic.game-type';
 import tournamentGameType from './games-types/tournament.game-type';
 import fpeGameType from './games-types/fpe.game-type';
 import { IGameType } from '@thefirstspine/types-rest';
+import standardGameType from './games-types/standard.game-type';
 
 export class GamesTypesLibrary {
 
   static all(): IGameType[] {
     const ret: IGameType[] = [
-      quickGameType,
+      classicGameType(),
+      tournamentGameType(),
+      fpeGameType(),
+      standardGameType(),
+      quickGameType(),
     ];
-    ret.push(classicGameType());
-    ret.push(tournamentGameType());
-    ret.push(fpeGameType());
     return ret;
   }
 
