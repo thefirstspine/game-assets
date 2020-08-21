2756b52303c960253fc6698abf4421d0502f4730
thefirstspine <teddy@coretizone.com>
2020-08-21 16:24:35 +0200

    Added new cycle

--- src/libraries/cycles.library.ts
+++ src/libraries/cycles.library.ts
@@ -53,8 +53,8 @@ export class CyclesLibrary {
         fr: `Pendant ce cycle pourfendez vos opposants et volez leurs armes. Dans tous les types de jeu, vous aurez des cartes "souvenirs" en fonction de la destinée de votre ennemi.`,
         en: `During this cycle slay your opponents and steal their weapons. In all types of game, you will have "souvenir" cards depending on the destiny of your enemy.`,
       },
-      timestampFrom: 0,
-      timestampTo: 0,
+      timestampFrom: (new Date('2020-07-24 15:00:00')).getTime() / 1000,
+      timestampTo: (new Date('2020-09-04 14:59:59')).getTime() / 1000,
     },
     {
       id: `harvest-2020`,
@@ -66,7 +66,7 @@ export class CyclesLibrary {
         fr: `Célébrez la Moisson en Exodia en participant au versus thématique de ce cycle : chaque créature tuée vous soignera !`,
         en: `Celebrate the Harvest in Exodia by participating in this cycle's thematic versus: each creature killed will heal you!`,
       },
-      timestampFrom: 0,
+      timestampFrom: (new Date('2020-09-04 14:59:00')).getTime() / 1000,
       timestampTo: (new Date('2020-10-16 14:59:59')).getTime() / 1000,
     },
   ];
