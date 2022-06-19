ce08395128dfb111c699a2c6a7ca6702c381f5fd
thefirstspine <teddy@coretizone.com>
2022-06-19 15:49:23 +0200

    Added new cards

---
+++ src/libraries/cards/argento-barbed-wires.card.ts
@@ -0,0 +1,42 @@
+import { ICard } from '@thefirstspine/types-rest';
+
+// tslint:disable: max-line-length
+const argentoBarbedWiresCard: ICard = {
+  id: `argento-barbed-wires`,
+  text: {
+    en: ``,
+    fr: ``,
+  },
+  lore: {
+    en: ``,
+    fr: ``,
+  },
+  art: 'Maylhine, Teddy Gandon',
+  imageUrl: `https://static.thefirstspine.fr/argento-barbed-wires.png`,
+  name: {
+    en: `Argento's Barbed wire`,
+    fr: `Barbelés d'Argento`,
+  },
+  stats: {
+    life: 1,
+    bottom: {
+      defense: 2,
+      strength: 1,
+    },
+    left: {
+      defense: 1,
+      strength: 3,
+    },
+    right: {
+      defense: 1,
+      strength: 3,
+    },
+    top: {
+      defense: 1,
+      strength: 4,
+    },
+  },
+  type: 'artifact',
+};
+
+export default argentoBarbedWiresCard;
---
+++ src/libraries/cards/insane-putrefaction.ts
@@ -0,0 +1,23 @@
+import { ICard } from '@thefirstspine/types-rest';
+
+// tslint:disable: max-line-length
+const insanePutrefactionCard: ICard = {
+  id: `insane-putrefaction`,
+  text: {
+    en: `The {creature}creature{/creature} loses {life}4{/life}.`,
+    fr: `La {creature}créature{/creature} perd {life}4{/life}.`,
+  },
+  lore: {
+    en: ``,
+    fr: ``,
+  },
+  imageUrl: `https://static.thefirstspine.fr/insane-putrefaction.png`,
+  name: {
+    en: `Insane's Putrefaction`,
+    fr: `Putréfaction de Démence`,
+  },
+  type: 'spell',
+  art: 'Maylhine, Teddy Gandon',
+};
+
+export default insanePutrefactionCard;
